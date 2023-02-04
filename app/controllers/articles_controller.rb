ed22fa3169c683f225ed69e9bd28bb00b2f7208b
arfaoui-khaled <arfaoui.kld@gmail.com>
2023-02-04 20:07:24 +0100

    updating aticle fonctionality added and using partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,20 @@ class ArticlesController < ApplicationController
      render :new, status: :unprocessable_entity	
 	end
   end	
+  
+  def edit
+    @article = Article.find(params(:id))
+  end
+  
+  def update
+    @article.find(params(:id))
+	
+	if @article.update(article_params)
+	  redirect_to @article
+	else
+	  render :edit, status: :unprocessable_entity
+	end
+  end
 	
    private
      def article_params
