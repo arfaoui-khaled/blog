ebcdef30390235be5f1fae8e41fcfbc46684bb43
arfaoui-khaled <arfaoui.kld@gmail.com>
2023-02-04 21:54:04 +0100

    generating a new model, associating models, executing migrations, adding routes, generating controllers, create controller action create for comments, updating views

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
  root "articles#index" 
  # get "/articles", to: "articles#index"
  # get "/articles/:id", to: "articles#show"
- resources :articles
+ #resources :articles
+ 
+ resources :articles do
+    resources :comments
+  end
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
