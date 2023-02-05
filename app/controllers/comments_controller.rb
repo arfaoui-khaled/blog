775cf433c9e35ba6a438db674436a3db0ce94da8
arfaoui-khaled <arfaoui.kld@gmail.com>
2023-02-05 06:51:32 +0100

    adding delete comment and associated objects functionality

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,14 @@ def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
-  end
+end
+  
+def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article), status: :see_other
+end
 
   private
     def comment_params
