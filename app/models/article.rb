a0492a70a5731bea3cda55e999cb0b4392df726f
arfaoui-khaled <arfaoui.kld@gmail.com>
2023-02-05 06:23:49 +0100

    fixing some bugs

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,13 +6,14 @@ class Article < ApplicationRecord
  validates :title, presence: true
  validates :body, presence: true, length: { minimum: 10 }
  
- =begin
- VALID_STATUSES = ['public', 'private', 'archived']
+ 
+ #VALID_STATUSES = ['public', 'private', 'archived']
 
-  validates :status, inclusion: { in: VALID_STATUSES }
+ #validates :status, inclusion: { in: VALID_STATUSES }
 
-  def archived?
-    status == 'archived'
-  end
-  =end
+  #def archived?
+  #  status == 'archived'
+  #end
+  
+ 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,14 @@
 class Comment < ApplicationRecord
-include Visible
+  include Visible
   belongs_to :article
   
-  =begin
-  VALID_STATUSES = ['public', 'private', 'archived']
+  
+  #VALID_STATUSES = ['public', 'private', 'archived']
 
-  validates :status, inclusion: { in: VALID_STATUSES }
+  #validates :status, inclusion: { in: VALID_STATUSES }
 
-  def archived?
-    status == 'archived'
-  end
-  =end
+  #def archived?
+  #  status == 'archived'
+  #end
+  
 end
